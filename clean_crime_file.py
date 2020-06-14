3268cebe8eda3ef9c3ed6e7a353f3c4e09fe4370
Sachin Myneni <myneni@altair.com>
2020-06-14 09:06:32 -0700

    filter

--- clean_crime_file.py
+++ clean_crime_file.py
@@ -2,6 +2,23 @@ import pandas as pd
 import re
 import logging
 
+def badrow(address: str, city: str) -> bool:
+    if city.split('_')[0].lower() in address.lower():
+        return False
+    else:
+        return True
+
 logging.basicConfig(filename="spotcrime_scrape.log", level=logging.DEBUG,
                     filemode='a', format='%(asctime)s %(message)s')
 
+crime_file = './spotcrime.csv.bad'
+
+try:
+    spotcrime_df = pd.read_csv(crime_file, header=0)
+    print(spotcrime_df.head())
+except FileNotFoundError:
+    logging.fatal(f"{crime_file} not found. Will create new one")
+except pd.errors.EmptyDataError:
+    logging.fatal(f"{crime_file} had no data. Renaming and will create new one")
+
+spotcrime_df.filter(badrow(spotcrime_df['ADDRESS'],spotcrime_df['CITY']))
