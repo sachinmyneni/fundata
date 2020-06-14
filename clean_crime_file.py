94cbc096725e2dcaa6b2d7223009cf084eaf3485
Sachin Myneni <myneni@altair.com>
2020-06-14 08:29:54 -0700

    first commit of crime_file cleanup

---
+++ clean_crime_file.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import re
+import logging
+
+logging.basicConfig(filename="spotcrime_scrape.log", level=logging.DEBUG,
+                    filemode='a', format='%(asctime)s %(message)s')
+
