d971b818ca0939cfa3488fd7d4c860f0b9958827
Sachin Myneni <myneni@altair.com>
2020-06-14 13:01:15 -0700

    filtering... now creates a 0 column DF for axis=1 and fails for axis=0

--- clean_crime_file.py
+++ clean_crime_file.py
@@ -8,17 +8,32 @@ def badrow(address: str, city: str) -> bool:
     else:
         return True
 
-logging.basicConfig(filename="spotcrime_scrape.log", level=logging.DEBUG,
-                    filemode='a', format='%(asctime)s %(message)s')
+# logging.basicConfig(filename="spotcrime_scrape.log", level=logging.DEBUG,
+#                     filemode='a', format='%(asctime)s %(message)s')
 
-crime_file = './spotcrime.csv.bad'
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
+crime_file = './spotcrime.csv.2'
 
 try:
     spotcrime_df = pd.read_csv(crime_file, header=0)
     print(spotcrime_df.head())
 except FileNotFoundError:
-    logging.fatal(f"{crime_file} not found. Will create new one")
+    logging.fatal(f"{crime_file} not found.")
 except pd.errors.EmptyDataError:
-    logging.fatal(f"{crime_file} had no data. Renaming and will create new one")
+    logging.fatal(f"{crime_file} had no data.")
 
-spotcrime_df.filter(badrow(spotcrime_df['ADDRESS'],spotcrime_df['CITY']))
+print(f"Shape before filter: {spotcrime_df.shape}")
+
+# spotcrime_df.filter(badrow(spotcrime_df['Address'],spotcrime_df['Place']))
+df_new = spotcrime_df.apply(lambda x: x['Place'].split('_')[0].lower() in x['Address'].lower(), axis=0)
+
+print(f"Shape after filter: {df_new.shape}")
